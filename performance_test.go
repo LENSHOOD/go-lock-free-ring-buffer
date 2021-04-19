8a7ba11f2c762c15f819fd16b7fa1ddd4ee5ed82
LENSHOOD <lenshood.zxh@gmail.com>
2021-04-19 16:48:02 +0800

    test: add benchmark

---
+++ performance_test.go
@@ -0,0 +1,109 @@
+package go_mpsc_ring_buffer
+
+import (
+	"math/rand"
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func setup() []int {
+	ints := make([]int, 64)
+	for i := 0; i < len(ints); i++ {
+		ints[i] = rand.Int()
+	}
+
+	return ints
+}
+
+type fakeBuffer struct {
+	capacity uint64
+	ch chan interface{}
+}
+
+func newFakeBuffer(capacity uint64) RingBuffer {
+	return &fakeBuffer{
+		capacity,
+		make(chan interface{}, capacity),
+	}
+}
+
+func (r *fakeBuffer) Offer(value interface{}) (success bool) {
+	select {
+	case r.ch <- value:
+		return true
+	default:
+		return false
+	}
+}
+
+func (r *fakeBuffer) Poll() (value interface{}, success bool) {
+	select {
+	case v := <-r.ch:
+		return v, true
+	default:
+		return nil, false
+	}
+}
+
+var controlCh = make(chan bool)
+var wg sync.WaitGroup
+func manage(b *testing.B, threadCount int, trueCount int) {
+	wg.Add(1)
+	for i := 0; i < threadCount; i++ {
+		if trueCount > 0 {
+			controlCh <- true
+			trueCount--
+		} else {
+			controlCh <- false
+		}
+	}
+
+	b.ResetTimer()
+	wg.Done()
+}
+
+func baseBenchmark(b *testing.B, buffer RingBuffer, threadCount int, trueCount int) {
+	ints := setup()
+
+	counter := int32(0)
+	go manage(b, threadCount, trueCount)
+	b.RunParallel(func(pb *testing.PB) {
+		producer := <-controlCh
+		wg.Wait()
+		for i := 1; pb.Next(); i++ {
+			if producer {
+				buffer.Offer(ints[(i & (len(ints) - 1))])
+			} else {
+				if _, success := buffer.Poll(); success {
+					atomic.AddInt32(&counter, 1)
+				}
+			}
+		}
+	})
+
+	b.StopTimer()
+	b.Logf("Success handover count: %d", counter)
+}
+
+
+func BenchmarkMPMC(b *testing.B) {
+	mpmcRB := New(MPMC, 16)
+	baseBenchmark(b, mpmcRB, runtime.GOMAXPROCS(0), runtime.GOMAXPROCS(0) / 2)
+}
+
+func BenchmarkChannelMPMC(b *testing.B) {
+	fakeB := newFakeBuffer(16)
+	baseBenchmark(b, fakeB, runtime.GOMAXPROCS(0), runtime.GOMAXPROCS(0) / 2)
+}
+
+func BenchmarkMPSC(b *testing.B) {
+	mpscRB := New(MPSC, 16)
+	baseBenchmark(b, mpscRB, runtime.GOMAXPROCS(0), runtime.GOMAXPROCS(0) - 1)
+}
+
+func BenchmarkChannelMPSC(b *testing.B) {
+	fakeB := newFakeBuffer(16)
+	baseBenchmark(b, fakeB, runtime.GOMAXPROCS(0), runtime.GOMAXPROCS(0) - 1)
+}
