640d868af49b8cb775f258ac783934b7d7ee198b
zhangxuhai <lenshood.zxh@gmail.com>
2022-08-16 10:32:28 +0800

    fix: bench script and add gen-report to makefile

--- bench/run-bench-mpmc.sh
+++ bench/run-bench-mpmc.sh
@@ -11,10 +11,11 @@ export LFRING_BENCH_CHARTS_FILE=table_define.dat
 cat /dev/null > $LFRING_BENCH_CHARTS_FILE
 
 ### bench with capacity
-echo \#title=with capacity\(threads=$LFRING_BENCH_THREAD_NUM, producers=$LFRING_BENCH_PRODUCER_NUM\),xAxis=capacity,yAxis=handover counts >> $LFRING_BENCH_CHARTS_FILE
-
 export LFRING_BENCH_THREAD_NUM=12
 export LFRING_BENCH_PRODUCER_NUM=6
+
+echo \#title=with capacity\(threads=$LFRING_BENCH_THREAD_NUM, producers=$LFRING_BENCH_PRODUCER_NUM\),xAxis=capacity,yAxis=handover counts >> $LFRING_BENCH_CHARTS_FILE
+
 capacities=(2 4 8 16 32 64 128 256 512 1024)
 for cap in "${capacities[@]}"
 do
@@ -25,9 +26,10 @@ done
 echo \#end >> $LFRING_BENCH_CHARTS_FILE
 
 ### bench with threads
+export LFRING_BENCH_CAP=32
+
 echo \#title=with thread number\(capacity=$LFRING_BENCH_CAP, producers=0.5*threads\),xAxis=threads,yAxis=handover counts >> $LFRING_BENCH_CHARTS_FILE
 
-export LFRING_BENCH_CAP=32
 threads=(2 4 8 12 24 48)
 for t in "${threads[@]}"
 do
@@ -39,10 +41,11 @@ done
 echo \#end >> $LFRING_BENCH_CHARTS_FILE
 
 ### bench with producer consumer ratio
-echo \#title=with producer\(capacity=$LFRING_BENCH_CAP, threads=$LFRING_BENCH_THREAD_NUM\),xAxis=producers,yAxis=handover counts >> $LFRING_BENCH_CHARTS_FILE
-
 export LFRING_BENCH_CAP=32
 export LFRING_BENCH_THREAD_NUM=12
+
+echo \#title=with producer\(capacity=$LFRING_BENCH_CAP, threads=$LFRING_BENCH_THREAD_NUM\),xAxis=producers,yAxis=handover counts >> $LFRING_BENCH_CHARTS_FILE
+
 producers=(1 2 3 4 6 8 9 10 11)
 for p in "${producers[@]}"
 do
